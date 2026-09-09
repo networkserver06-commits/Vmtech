8ff16ad454f34f442f2a1e010899e2ee137f4723
networkserver06-commits <2.50408281e+08+networkserver06-commits@users.noreply.github.com>
2026-09-09 15:08:03 +0000

    Fix backend shared import for Vercel

--- server/routers.ts
+++ server/routers.ts
@@ -1,6 +1,6 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { COOKIE_NAME } from "@shared/const";
+import { COOKIE_NAME } from "../shared/const.js";
 import { getSessionCookieOptions } from "./_core/cookies.js";
 import { systemRouter } from "./_core/systemRouter.js";
 import { protectedProcedure, publicProcedure, router } from "./_core/trpc.js";
