846f4c5ea3b91c2681e869c8f80d750661a1b6aa
networkserver06-commits <networkserver06-commits@users.noreply.github.com>
2026-09-09 17:58:28 +0000

    Remove payout flow and fix dashboard hooks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import Collections from "./pages/Collections";
-import Payouts from "./pages/Payouts";
 import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import Landing from "./pages/Landing";
@@ -19,7 +18,6 @@ function Router() {
       <Route path="/dashboard" component={Home} />
       <Route path="/admin" component={Admin} />
       <Route path="/collections" component={Collections} />
-      <Route path="/payouts" component={Payouts} />
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
